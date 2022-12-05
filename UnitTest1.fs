7912e0cc2f2a12da5b05ef86b330f99f8af3b4f5
BDA7 <bondare-danila2017@yandex.ru>
2022-12-05 03:57:25 +0300

    add tests

--- UnitTest1.fs
+++ UnitTest1.fs
@@ -5,25 +5,34 @@ open Functions
 
 [<SetUp>]
 let Setup () = ()
+let values = [|12; 220; 224; 644; 532; 1000; 2256|]
+let results1 = [|60; 199980; 268800; 8027460; 5290740; 31875000; 57336240|]
+let result2 = 870873746
 
 [<TestFixture>]
 type TestClass() =
     [<Test>]
-    member this.Test1() =
-        assert (findTriplet (12) = 60)
-        assert (recursionFind (12) = 60)
-        assert (findTriplet (644) = 8027460)
-        assert (recursionFind (644) = 8027460)
-        assert (findTriplet (1000) = 31875000)
-        assert (recursionFind (1000) = 31875000)
-        assert (findTriplet (2256) = 57336240)
-        assert (recursionFind (2256) = 57336240)
-        Assert.Pass()
+    member this.``Test find triplet``() =
+        for i=0 to values.Length-1 do
+            assert (findTriplet (values[i]) = results1[i])
 
     [<Test>]
-    member this.Test2() =
-        assert (mapCounter = 870873746)
-        assert (counterNames = 870873746)
-        assert (recursionCounter = 870873746)
-        assert (tailRecursionCounter = 870873746)
-        Assert.Pass()
+    member this.``Test tail recursion triplet``() =
+        for i=0 to values.Length-1 do
+            assert (recursionFind (values[i]) = results1[i])
+            
+    [<Test>]
+    member this.``Test counter names``() =
+        assert (counterNames = result2)
+    
+    [<Test>]
+    member this.``Test map counter``() =
+        assert (mapCounter = result2)
+    
+    [<Test>]
+    member this.``Test recursion counter``() =
+        assert (recursionCounter = result2)
+
+    [<Test>]
+    member this.``Test tail recursion counter``() =
+        assert (tailRecursionCounter = result2)
